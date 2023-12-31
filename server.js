2224ed0d80cfface758c92f2c2ba7456a844754e
ManManavadaria <manmanavadaria@gmail.com>
2023-12-31 13:26:05 +0530

    cors edit

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 5000;
 
 // middleware to get parsed json data from request
 // app.use(cors());
-app.use(cors({ origin: process.env.ORIGIN}));
-
+// app.use(cors({ origin: process.env.ORIGIN}));
+app.use(cors({ origin: 'https://frontend-emmar.vercel.app/' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use("/", checkApi());
